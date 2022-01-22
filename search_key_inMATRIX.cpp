3546f087be0986ab061aec02e9c930b7b6b4669b
kalyan14-cloud <61815993+kalyan14-cloud@users.noreply.github.com>
2022-01-22 14:27:01 +0530

    search key in matrix

---
+++ search_key_inMATRIX.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+signed main(){
+	int n,m;
+	cin>>n>>m;
+	int arr[n][m];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			cin>>arr[i][j];
+		}
+	}
+	int x;
+	cin>>x;
+    
+    //start from right top corner of matrix
+	int idx1=0,idx2=m-1;
+	bool flag=false;
+	while(idx1<n and idx2>=0){
+		if(arr[idx1][idx2]==x){
+			flag=true;
+			break;
+		}
+		else if(arr[idx1][idx2]<x){
+			idx1++;
+		}
+		else if(arr[idx1][idx2]>x){
+			idx2--;
+		}
+	}
+	
+	flag==true?cout<<1<<endl : cout<<0<<endl;
+}
