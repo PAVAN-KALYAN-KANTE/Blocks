3e22e4feca03da2e2e8230f066d96ed28ac41271
kalyan14-cloud <61815993+kalyan14-cloud@users.noreply.github.com>
2022-01-22 11:01:08 +0530

    demo4

---
+++ demo4.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+signed main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(auto &i:arr){
+        cin>>i;
+    }
+    int target;
+    cin>>target;
+
+    sort(arr,arr+n);
+    int left=0,right=n-1;
+    while(left<right){
+        if(arr[left]+arr[right]==target){
+            cout<<arr[left]<<" and "<<arr[right]<<endl;
+            left++;
+            right--;
+        }
+        else if(arr[left]+arr[right]>target){
+            right--;
+        }
+        else if(arr[left]+arr[right]<target){
+            left++;
+        }
+    }
+}
