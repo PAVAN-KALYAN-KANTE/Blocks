596482fd3016a608de3ceb7dcca41df7067322f5
kalyan14-cloud <61815993+kalyan14-cloud@users.noreply.github.com>
2022-01-22 16:10:58 +0530

    smallest number formed by inverting digit with 9

--- smallest_num_byInverting_with(.cpp
+++ smallest_num_byInverting_with(.cpp
@@ -1,26 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 signed main(){
 	long long int x;
 	cin>>x;
-	string y="";
-	while(x>0){
-		int rem=x%10;
-
-		if(9-rem<rem){
-           y=to_string(9-rem)+y;
+	string y=to_string(x);
+//	cout<<y<<"  "<<y.length()<<endl;
+	for(int i=0;i<y.length();i++){
+		auto temp=(9-y[i]+48);
+		//cout<<temp<<endl;
+		if(y[i]-48>temp){
+			y[i]=temp+48;
 		}
-		else{
-			y=to_string(rem)+y;
-		}
-
-		x=x/10;
 	}
-
 	if(y[0]=='0'){
 		y[0]='9';
 	}
-    cout<<y<<endl;
-	
+	cout<<y<<endl;
 	
 }
+// signed main(){
+// 	long long int x;
+// 	cin>>x;
+// 	string y="";
+// 	while(x>0){
+// 		int rem=x%10;
+
+// 		if(9-rem<rem){
+//            y=to_string(9-rem)+y;
+// 		}
+// 		else{
+// 			y=to_string(rem)+y;
+// 		}
+
+// 		x=x/10;
+// 	}
+
+// 	if(y[0]=='0'){
+// 		y[0]='9';
+// 	}
+//     cout<<y<<endl;
+	
+	
+// }
